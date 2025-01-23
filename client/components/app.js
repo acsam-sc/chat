0fe3894eecd30e618ba72b9aa04f5045b5b56ef4
acsam-sc <acsam42@gmail.com>
2025-01-23 01:35:22 +0100

    fix: attempt to fix scroll in mobile version

--- client/components/app.js
+++ client/components/app.js
@@ -4,7 +4,8 @@ import Body from './body'
 
 const App = () => {
   return (
-    <div className="w-full h-screen flex border shadow bg-white">
+    // <div className="w-full h-screen flex border shadow bg-white">
+    <div className="w-full absolute inset-0 flex border shadow bg-white">
       <Route exact path="/chat" component={() => <Body />} />
     </div>
   )
